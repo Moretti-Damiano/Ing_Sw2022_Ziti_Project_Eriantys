2ff6634eec92f0313099083c4487c2425cd87898
simonecignoli <simone.cignoli@mail.polimi.it>
2022-04-05 23:11:05 +0200

    fixed board, player and walletcontroller

--- src/main/java/it/polimi/ingsw/Ziti/launcher/model/Board.java
+++ src/main/java/it/polimi/ingsw/Ziti/launcher/model/Board.java
@@ -1,4 +1,4 @@
-package it.polimi.ingsw.Ziti.launcher;
+package it.polimi.ingsw.Ziti.launcher.model;
 
 import java.util.ArrayList;
 
@@ -10,7 +10,7 @@ public class Board {
         private ArrayList<Professor> professors;
         private ArrayList<Student>[] students;
         private ArrayList<Coin> wallet;
-        private WalletController walletController;
+
 
 
         public Board() {
@@ -24,6 +24,15 @@ public class Board {
             }
         }
 
+    public ArrayList<Professor> getProfessors(){ return this.professors;}
+
+    /**
+     * @param s student to add
+     */
+    public void addStudent(Student s){
+            students_waiting.add(s);
+    }
+
 
     public void setTowerColour(TowerColour tower_colour) {
         this.tower_colour = tower_colour;
@@ -34,9 +43,7 @@ public class Board {
      * Removes a tower from towers and returns it
      */
     public Tower removeTower() {
-        int index = (int) (Math.random() * towers.size());
-        return towers.get(index);
-        towers.remove(index);
+       return towers.remove(0);
     }
 
 
@@ -51,7 +58,7 @@ public class Board {
 
     //getintabbreviation dovrebbe restituire l'intero associato al colore
     public void goLunch(Colour student_colour) {
-        students[student_colour.getintabbreviation()] .add(this.leave(student_colour));
+        students[student_colour.getIntAbbreviation()] .add(this.leave(student_colour));
         if(this.checkCoin(student_colour)){
             wallet.add(walletController.getCoin());
         }
@@ -59,21 +66,20 @@ public class Board {
 
 
     public Student leave(Colour student_colour){
-        ArrayList<Student> s_leave=new ArrayList<Student>();
         for(Student s : students_waiting){
             if(s.getColour()==student_colour){
-                s_leave.add(s);
+               students_waiting.remove(s);
+               return s;
             }
         }
-        int index = (int) (Math.random() * s_leave.size());
-        return s_leave.get(index);
-        students_waiting.remove(s_leave.get(index));
+        return null;
+
     }
 
 
 
     private boolean checkCoin(Colour student_colour){
-        if(students[student_colour.getintabbreviation].size() % 3==0){
+        if(students[student_colour.getIntAbbreviation()].size() % 3==0){
             return true;
         }
         return false;
