70670084d36e8fea25e8dcd26c1db849536ae954
Alessio <alessio.lanzini@mail.polimi.it>
2022-04-05 10:04:59 +0200

    MoveMother idea

--- src/main/java/it/polimi/ingsw/Ziti/launcher/action/MoveMother.java
+++ src/main/java/it/polimi/ingsw/Ziti/launcher/action/MoveMother.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.Ziti.launcher.action;
 
-import it.polimi.ingsw.Ziti.launcher.model.Game;
+import it.polimi.ingsw.Ziti.launcher.model.*;
 
 public class MoveMother implements Action{
     private Game game;
@@ -13,6 +13,34 @@ public class MoveMother implements Action{
 
     @Override
     public Object execute() {
+
+    }
+
+    /**
+     *
+     * @param island
+     * @return the player with most influence on island
+     */
+    public Player getControl(Island island){
+
+    }
+
+    /**
+     * Merges two islands into one
+     * @param  island1 is the 'main' island that will be updated
+     * @param island2 will be deleted after the merge
+     */
+    public void merge(Island island1, Island island2){
+
+    }
+
+    /**
+     * checks if island1 and island2 can be merged
+     * @param island1
+     * @param island2
+     * @return true if the islands can be merged, else false
+     */
+    public boolean checkMerge(Island island1, Island island2){
 
     }
 }
