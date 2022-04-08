006314bba4a61d76b6acc3422de0388cbca3e7a8
Alessio <alessio.lanzini@mail.polimi.it>
2022-04-08 18:34:25 +0200

    Minor changes

--- src/main/java/it/polimi/ingsw/Ziti/launcher/action/EndTurn.java
+++ src/main/java/it/polimi/ingsw/Ziti/launcher/action/EndTurn.java
@@ -1,9 +1,13 @@
 package it.polimi.ingsw.Ziti.launcher.action;
+import it.polimi.ingsw.Ziti.launcher.exception.ActionException;
 import it.polimi.ingsw.Ziti.launcher.model.*;
 
 public class EndTurn implements Action{
 
    private Game game;
 
+   @Override
+   public void execute() throws ActionException {
 
+   }
 }
