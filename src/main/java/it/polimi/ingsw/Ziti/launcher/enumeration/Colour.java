2ebec04f42937cb4e53729fea3eec07b444cabbf
Alessio <alessio.lanzini@mail.polimi.it>
2022-04-22 15:16:39 +0200

    Added name as an abbreviation

--- src/main/java/it/polimi/ingsw/Ziti/launcher/enumeration/Colour.java
+++ src/main/java/it/polimi/ingsw/Ziti/launcher/enumeration/Colour.java
@@ -8,21 +8,20 @@ import java.util.Map;
  *
  * */
 public enum Colour {
-    BLUE("0"),
-    GREEN("1"),
-    PINK("2"),
-    RED("3"),
-    YELLOW("4");
+    BLUE("0","blue"),
+    GREEN("1","green"),
+    PINK("2","pink"),
+    RED("3","red"),
+    YELLOW("4","yellow");
 
     private String abbreviation;
-
+    private String name;
 
     /**
      * The set of Colours addressed by abbreviations.
      */
     private static final Map<String, Colour> COLOURS_BY_ABBR = new HashMap<String, Colour>();
-
-
+    private static final Map<String, Colour> COLOURS_BY_NAME = new HashMap<String, Colour>();
 
     /**
      * Static initializer
@@ -31,13 +30,17 @@ public enum Colour {
         for (Colour colour : values()) {
             COLOURS_BY_ABBR.put(colour.getAbbreviation(), colour);
         }
+        for(Colour colour : values()){
+            COLOURS_BY_NAME.put(colour.getName(), colour);
+        }
     }
 
 
     /**
      * Default constructor.
      */
-    Colour(String abbreviation) {
+    Colour(String abbreviation, String name) {
+        this.name = name;
         this.abbreviation = abbreviation;
     }
 
@@ -47,6 +50,10 @@ public enum Colour {
 
     public String getAbbreviation() {return abbreviation;}
 
+    public String getName() {
+        return name;
+    }
+
     /**
      * Gets the enum constant with the specified abbreviation.
      *
@@ -55,6 +62,17 @@ public enum Colour {
      */
     public static Colour valueOfAbbreviation(final String abbr) {
         final Colour colour = COLOURS_BY_ABBR.get(abbr);
+        return colour;
+    }
+
+    /**
+     * Gets the enum constant with the specified name.
+     *
+     * @param name the colour's name.
+     * @return the enum constant with the specified name.
+     */
+    public static Colour valueOfName(final String name) {
+        final Colour colour = COLOURS_BY_ABBR.get(name);
         return colour;
     }
 
