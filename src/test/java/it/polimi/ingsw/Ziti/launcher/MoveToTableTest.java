acef0f11e5672f10366d3b031c0073c051fd9263
simonecignoli <simone.cignoli@mail.polimi.it>
2022-04-25 12:40:22 +0200

    fixed MoveToTableTest

--- src/test/java/it/polimi/ingsw/Ziti/launcher/MoveToTableTest.java
+++ src/test/java/it/polimi/ingsw/Ziti/launcher/MoveToTableTest.java
@@ -52,7 +52,7 @@ public class MoveToTableTest {
            p1.getBoard().addStudent(new Student(c));
        }
        p1.getBoard().addProfessor(game.getProfessorbyColour(Colour.RED));
-        game.setAction(new MoveToTable(game,Colour.RED));
+        game.setAction(new MoveToTable(game,"red"));
         game.doAction();
        assertTrue(p0.getBoard().hasProfessor(Colour.RED));
        assertFalse(p1.getBoard().hasProfessor(Colour.RED));
@@ -60,7 +60,7 @@ public class MoveToTableTest {
        for(int i=0;i<2;i++){
            p0.getBoard().addStudenttoColourRow(new Student(Colour.BLUE));
        }
-       game.setAction(new MoveToTable(game,Colour.BLUE));
+       game.setAction(new MoveToTable(game,"blue"));
        game.doAction();
        assertTrue(p0.getBoard().hasProfessor(Colour.BLUE));
        assertEquals(1,p0.getBoard().getNumberofCoin());
@@ -68,7 +68,7 @@ public class MoveToTableTest {
        p1.getBoard().addStudenttoColourRow(new Student(Colour.PINK));
        p1.getBoard().addStudenttoColourRow(new Student(Colour.PINK));
        p1.getBoard().addProfessor(new Professor(Colour.PINK));
-        game.setAction(new MoveToTable(game,Colour.PINK));
+        game.setAction(new MoveToTable(game,"pink"));
         game.doAction();
         assertFalse(p0.getBoard().hasProfessor(Colour.PINK));
         assertEquals(7,p0.getBoard().getColorRowSize(Colour.PINK));
