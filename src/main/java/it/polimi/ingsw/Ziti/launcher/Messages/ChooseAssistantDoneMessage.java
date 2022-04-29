1a512d8760392fb55ee142e3661ee4720460dd01
simonecignoli <simone.cignoli@mail.polimi.it>
2022-04-29 11:41:38 +0200

    implemented new messages

--- src/main/java/it/polimi/ingsw/Ziti/launcher/Messages/ChooseAssistantDoneMessage.java
+++ src/main/java/it/polimi/ingsw/Ziti/launcher/Messages/ChooseAssistantDoneMessage.java
@@ -5,10 +5,21 @@ import it.polimi.ingsw.Ziti.launcher.model.Assistant;
 public class ChooseAssistantDoneMessage extends ActionMessage{
     private Assistant assistant;
     private String playername;
+
     public ChooseAssistantDoneMessage(String description, Assistant assistant, String playername) {
         super(description);
         this.assistant=assistant;
         this.playername=playername;
+        }
 
+    public Assistant getAssistant() {
+        return assistant;
     }
+
+    public String getPlayername() {
+        return playername;
+    }
+
+
 }
+
