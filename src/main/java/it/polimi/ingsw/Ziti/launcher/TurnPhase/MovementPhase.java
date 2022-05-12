b2b2d93ddeb6e9fbe87398f69bea2900057cd8c9
simonecignoli <simone.cignoli@mail.polimi.it>
2022-05-12 20:15:24 +0200

    added a movement

--- src/main/java/it/polimi/ingsw/Ziti/launcher/TurnPhase/MovementPhase.java
+++ src/main/java/it/polimi/ingsw/Ziti/launcher/TurnPhase/MovementPhase.java
@@ -6,16 +6,26 @@ import it.polimi.ingsw.Ziti.launcher.enumeration.PhaseType;
 public class MovementPhase extends Phase{
 
     private int moveNumber;
+    private int numplayer;
 
     public MovementPhase(TurnController turnController) {
         super(turnController, PhaseType.MOVEMENT);
         moveNumber = 0;
+        numplayer=turnController.getPlayers().size();
     }
 
     @Override
     public void update() {
         endCharacter();
         moveNumber++;
+        if (numplayer == 3) {
+            if (moveNumber == 4) {
+                moveNumber = 0;
+                nextPhase();
+            } else {
+                checkCharacter();
+            }
+        } else {
             // Check if the player has already moved 3 students (ToTable or ToIsland)
             if (moveNumber == 3) {
                 moveNumber = 0;
@@ -23,7 +33,7 @@ public class MovementPhase extends Phase{
             } else {
                 checkCharacter();
             }
-
+        }
     }
     @Override
     public void nextPhase() {
