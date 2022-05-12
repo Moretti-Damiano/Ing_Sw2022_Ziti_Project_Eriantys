e0a3fc2f5238bc0a80e2c1cc34af5d7a2780e737
Simone Lavermicocca <simone.lavermicocca@mail.polimi.it>
2022-05-12 15:11:18 +0200

    ChooseCloudScene created

---
+++ src/main/java/it/polimi/ingsw/Ziti/launcher/view/gui/scene/ChooseCloudSceneController.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.Ziti.launcher.view.gui.scene;
+
+import it.polimi.ingsw.Ziti.launcher.observer.InputObservable;
+
+public class ChooseCloudSceneController extends InputObservable implements GenericSceneController{
+}
